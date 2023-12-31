1521ef5f19ef3d0875248b0b59ae03883cb6dd62
LongNgo35 <118904539+LongNgo35@users.noreply.github.com>
2023-12-31 23:52:54 +0700

    Add files via upload

---
+++ lib_hello_world_git_repos.c
@@ -0,0 +1,6 @@
+#include "lib_hello_world_git_repos.h"
+
+void hello_world_printing(void)
+{
+	printf("hello world - called by lib function\n");
+}
---
+++ lib_hello_world_git_repos.h
@@ -0,0 +1,11 @@
+#ifndef __LIB_HELLO_WORLD__
+#define __LIB_HELLO_WORLD__
+
+#include <stdio.h>
+
+/* prototype */
+void hello_world_printing(void);
+
+#endif
+
+
